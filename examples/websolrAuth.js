3526a41a9f175bf44cfd3bddb03ace21a06c4d50
Andres Citroni <acitroni@growthaccelerationpartners.com>
2017-10-27 11:18:18 -0300

    Add WebSolr Advanced Auth

---
+++ examples/websolrAuth.js
@@ -0,0 +1,42 @@
+/**
+ * Add documents into the Solr index.
+ */
+
+// Use `var solr = require('solr-client')` in your code 
+var solr = require('./../lib/solr'); 
+
+// Create a client
+var client = solr.createClient();
+
+// Set WebSolr Auth
+var token = 'ABC-123';
+client.setWebsolrAdvancedAuth('token')
+
+// Add extra headers
+var header = {
+    'extra': 'data',
+    'other-extra': 'more data'
+};
+client.setHeaders(header);
+
+// Switch on "auto commit", by default `client.autoCommit = false`
+client.autoCommit = true;
+
+var docs = [];
+for(var i = 0; i <= 10 ; i++){
+   var doc = {
+       id : 12345 + i,
+       title_t : "Title "+ i,
+       description_t : "Text"+ i + "Alice"
+   }
+   docs.push(doc);
+}
+
+// Add documents
+client.add(docs,function(err,obj){
+   if(err){
+      console.log(err);
+   }else{
+      console.log(obj);
+   }
+});
